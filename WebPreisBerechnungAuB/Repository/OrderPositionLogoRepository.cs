a1059603e5b6bba829bf72ca6bfe2bbff82fc3b8
m3xpl4y <m3xpl4y@gmail.com>
2022-10-02 22:24:54 +0200

    Repositories hinzugefügt 2x, Interface für Repositories hinzugefügt

---
+++ WebPreisBerechnungAuB/Repository/OrderPositionLogoRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebPreisBerechnungAuB.Data;
+using WebPreisBerechnungAuB.Models;
+using WebPreisBerechnungAuB.Services.Interface;
+
+namespace WebPreisBerechnungAuB.Repository
+{
+    public class OrderPositionLogoRepository : IRepository<OrderPositionLogo, int>
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IUserService _userService;
+
+        public OrderPositionLogoRepository(ApplicationDbContext context, IUserService userService)
+        {
+            _context = context;
+            _userService = userService;
+        }
+        public async Task CreateAsync(OrderPositionLogo model)
+        {
+            await _context.AddAsync(model);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var model = await _context.OrderPositionLogos.FindAsync(id);
+            _context.Remove(model);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<OrderPositionLogo>> GetAllAsync()
+        {
+            var user = await _userService.CurrentUser();
+            return await _context.OrderPositionLogos
+                .Include(x => x.Order)
+                .Include(x => x.Order.Textil)
+                .Include(x => x.Order.TextilColor)
+                .Include(x => x.User)
+                .Where(x => x.User == user)
+                .ToListAsync();
+        }
+
+        public async Task<OrderPositionLogo> GetByIdAsync(int id)
+        {
+            return await _context.OrderPositionLogos.FindAsync(id);
+        }
+
+        public async Task Update(OrderPositionLogo model)
+        {
+            _context.OrderPositionLogos.Update(model);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ WebPreisBerechnungAuB/Services/Interface/IRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebPreisBerechnungAuB.Services.Interface
+{
+    public interface IRepository<T1, T2> where T1 : class
+    {
+        /// <summary>
+        /// Is returning a IEnumerable of the class provided
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<T1>> GetAllAsync();
+        /// <summary>
+        /// Is returning a object by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<T1> GetByIdAsync(T2 id);
+        /// <summary>
+        /// Create a new object 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        Task CreateAsync(T1 model);
+        /// <summary>
+        /// Update the provided object
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        Task Update(T1 model);
+        /// <summary>
+        /// Delete the object by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task Delete(T2 id);
+    }
+}
