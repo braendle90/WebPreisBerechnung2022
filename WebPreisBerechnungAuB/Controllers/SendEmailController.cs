f5b0793fdc4d045febdfac417585d824ad00b6b0
braendle90 <d.braendle@aub.at>
2022-10-07 00:08:14 +0200

    Hard Implementierung E-Mail Sender

---
+++ WebPreisBerechnungAuB/Controllers/SendEmailController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Crypto.Tls;
+using System.Threading.Tasks;
+using WebPreisBerechnungAuB.Data;
+using WebPreisBerechnungAuB.Models;
+using WebPreisBerechnungAuB.Repo;
+
+namespace WebPreisBerechnungAuB.Controllers
+{
+    public class SendEmailController : Controller
+    {
+
+
+
+
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IGetFromDB _getFromDB;
+        private readonly Ihelper _helper;
+        private readonly IEmailSender _emailSender;
+
+
+
+        public SendEmailController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, IWebHostEnvironment webHostEnvironment)
+        {
+            this._context = context;
+            this._userManager = userManager;
+            this._webHostEnvironment = webHostEnvironment;
+            this._getFromDB = new GetFromDB(_context, userManager);
+            this._helper = new helper(_context, userManager);
+            
+        }
+        public async Task<IActionResult> SendOfferMail(string body)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var mail = new EMail();
+            mail.emailAdressTo = user.Email;
+            mail.htmlMessage = body;   
+            mail.subject = body;
+
+
+
+            var sender = new EmailSender();
+            sender.SendEmailAsync(mail);        
+
+
+            return View();
+        }
+    }
+}
--- WebPreisBerechnungAuB/EmailSender.cs
+++ WebPreisBerechnungAuB/EmailSender.cs
@@ -1,13 +1,30 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit.Text;
+using MimeKit;
 using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using WebPreisBerechnungAuB.Models;
 
 namespace WebPreisBerechnungAuB
 {
-    public class EmailSender : IEmailSender
+    public class EmailSender 
     {
-        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public async Task SendEmailAsync(EMail emailModel)
         {
-            throw new System.NotImplementedException();
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("ettie.reichel63@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse(emailModel.emailAdressTo));
+            email.Subject = emailModel.subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = emailModel.htmlMessage };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.ethereal.email", 587, MailKit.Security.SecureSocketOptions.StartTls);
+            smtp.Authenticate("ettie.reichel63@ethereal.email", "gsqNaxS4YRUdYbaePn");
+            smtp.Send(email);
+            smtp.Disconnect(true);
         }
     }
 }
+
+
+
