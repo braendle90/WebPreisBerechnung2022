0768379c683215f7c57d05853345ce03b8691a77
Dominik Brändle <80281828+braendle90@users.noreply.github.com>
2023-09-05 22:27:43 +0200

    update

---
+++ WebPreisBerechnungAuB/Controllers/TextilController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPreisBerechnungAuB.Controllers
+{
+    public class TextilController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebPreisBerechnungAuB/Properties/PublishProfiles/CreateZip.ps1
@@ -0,0 +1,6 @@
+﻿# CreateZip.ps1
+
+$sourcePath = "C:\Users\domin\source\repos\braendle90\WebPreisBerechnung2023\WebPreisBerechnungAuB\bin\Release\net7.0\publish"
+$zipPath = "C:\Users\domin\source\repos\braendle90\WebPreisBerechnung2023\WebPreisBerechnungAuB\bin\Release\Preisberechnung.zip"
+
+Compress-Archive -Path $sourcePath\* -DestinationPath $zipPath
