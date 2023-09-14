7b216e52d8bda84ccb612993854d863ede2f86d0
Dominik Brändle <80281828+braendle90@users.noreply.github.com>
2023-09-15 00:41:55 +0200

    update Textil Index site

--- WebPreisBerechnungAuB/Controllers/TextilController.cs
+++ WebPreisBerechnungAuB/Controllers/TextilController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.NetworkInformation;
 using WebPreisBerechnungAuB.Data;
 using WebPreisBerechnungAuB.Models;
@@ -30,11 +33,47 @@ namespace WebPreisBerechnungAuB.Controllers
         }
         public IActionResult Index()
         {
+            List<ArticelMain> mainArticelList = new List<ArticelMain>();
+
+
+            string textFile = "C:\\Users\\domin\\Downloads\\Articel_09_2023.csv";
+
+            // Read file using StreamReader. Reads file line by line
+            using (StreamReader file = new StreamReader(textFile))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    char[] spearator = { ',' };
+                    Int32 count = 3;
+
+                    // Using the Method
+                    String[] strlist = ln.Split(spearator,
+                           count, StringSplitOptions.None);
+
+               
+
+                    mainArticelList.Add(new ArticelMain(strlist[0], strlist[1], strlist[2]));
+
+                    if (counter == 10)
+                    {
+                        break;
+                    }
+
+                    //Console.WriteLine(ln);
+                    counter++;
+                }
+                file.Close();
+
+            }
+        
 
 
 
 
-            return View();
+            return View(mainArticelList);
         }
 
         public IActionResult DetailSite()
@@ -109,6 +148,45 @@ namespace WebPreisBerechnungAuB.Controllers
 
             return article;
 
+        }
+
+        public IActionResult fillDatabasefromExel() 
+        {
+
+            string textFile =  "C:\\Users\\domin\\Downloads\\Articel_09_2023.csv";
+
+            if (System.IO.File.Exists(textFile))
+            {
+                // Read file using StreamReader. Reads file line by line
+                using (StreamReader file = new StreamReader(textFile))
+                {
+                    int counter = 0;
+                    string ln;
+
+                    while ((ln = file.ReadLine()) != null)
+                    {
+                        char[] spearator = { ','};
+                        Int32 count = 3;
+
+                        // Using the Method
+                        String[] strlist = ln.Split(spearator,
+                               count, StringSplitOptions.None);
+
+                        foreach (String s in strlist)
+                        {
+                            Console.WriteLine(s);
+                        }
+
+
+                        //Console.WriteLine(ln);
+                        counter++;
+                    }
+                    file.Close();
+                   
+                }
+            }
+
+            return View("Index");
         }
 
 
---
+++ WebPreisBerechnungAuB/Models/ArticelMain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace WebPreisBerechnungAuB.Models
+{
+    public class ArticelMain
+    {
+
+        public ArticelMain() { }
+        public ArticelMain(string name, string brand, string articelNumber) 
+        {
+
+            Name = name;
+            Brand = brand;
+            ArticelNumber = articelNumber;
+
+        }
+
+        public int Id { get; set; }
+
+        public List<File> file { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }   
+
+        public string ArticelNumber { get; set; }   
+    }
+}
